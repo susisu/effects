2e49bbfba865c191849613b022481a6d6850f620
Susisu <susisu2413@yahoo.co.jp>
2020-05-05 14:12:20 +0900

    test: add test to check number of computation executed

--- src/array.test.ts
+++ src/array.test.ts
@@ -1,20 +1,26 @@
-import { Proc } from "./core";
+import { Proc, CoreEffKind, compute } from "./core";
 import { ArrayEffKind, runArray, split } from "./array";
 
 describe("array", () => {
   describe("runArray", () => {
     it("should run a procedure that can split", () => {
-      const proc1: Proc<ArrayEffKind, number> = perform => {
+      const f = jest.fn();
+      const g = jest.fn();
+      const proc1: Proc<ArrayEffKind | CoreEffKind, number> = perform => {
         const x = perform(split([1, 2]));
+        perform(compute(f));
         const y = perform(split([1, 3]));
+        perform(compute(g));
         return x * y;
       };
       const res1 = runArray(proc1);
       expect(res1).toEqual([1, 3, 2, 6]);
+      expect(f).toHaveBeenCalledTimes(2);
+      expect(g).toHaveBeenCalledTimes(4);
 
       const proc2: Proc<ArrayEffKind, number> = perform => {
         const x = perform(split([1, 2]));
-        const y = perform(split([]));
+        const y = perform(split<number>([]));
         return x * y;
       };
       const res2 = runArray(proc2);
